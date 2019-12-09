38f38536728ff7419a56725a1987c8496a447f8b
mkrill <michael.krill@telekom.de>
2019-12-09 12:58:35 +0100

    first commit

---
+++ src/test/java/de/telekom/Spring02Doctorwho/Spring02DoctorWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.Spring02Doctorwho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02DoctorWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
